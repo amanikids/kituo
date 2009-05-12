eaf60d33371255c31facf0993a80609a0ad365d8
Matthew Todd <matthew.todd@gmail.com>
2009-05-12 14:16:21 +0300

    Setup for localization.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.filter 'locale'
+
   map.resources :children, :collection => { :pending => :get, :onsite => :get, :boarding_offsite => :get, :dropped_out => :get, :reunified => :get, :terminated => :get } do |child|
     child.resource :headshot
     child.resources :arrivals, :offsite_boardings, :reunifications, :dropouts, :terminations
