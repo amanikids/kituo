88254cb65b184f911d4e6e47285d6b86cab5a6bb
Matthew Todd <matthew.todd@gmail.com>
2009-05-19 13:01:52 +0300

    Edit child headshot through the children controller, like caregivers.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :tasks, :collection => { :unrecorded_arrivals => :get, :upcoming_home_visits => :get }
 
-  map.resources :children, :collection => { :onsite => :get, :boarding_offsite => :get, :dropped_out => :get, :reunified => :get, :terminated => :get } do |child|
-    child.resource :headshot # FIXME delete the HeadshotsController and make headshot be a member action for a Child
+  map.resources :children, :collection => { :onsite => :get, :boarding_offsite => :get, :dropped_out => :get, :reunified => :get, :terminated => :get }, :member => { :headshot => :get } do |child|
     child.resources :arrivals, :home_visits, :offsite_boardings, :reunifications, :dropouts, :terminations
   end
 
