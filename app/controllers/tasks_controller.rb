c21eea251ec77f800e771b71db67853f7e8800e7
Matthew Todd <matthew.todd@gmail.com>
2009-05-22 11:00:15 +0300

    Let's prioritize the whole Social Worker assignment thing.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,16 @@
 class TasksController < ApplicationController
   def index
-    redirect_to unrecorded_arrivals_tasks_path, :skip_contextual_magic => true
+    redirect_to unassigned_children_tasks_path, :skip_contextual_magic => true
   end
 
   # TODO pull the mapping inside of Task
   # TODO can we do the same inside Caregiver?
-  def unrecorded_arrivals
-    @tasks = Child.unrecorded_arrivals.map { |child| Task.record_arrival(child) }.sort
-  end
-
   def unassigned_children
     @tasks = Child.without_social_worker.map { |child| Task.assign_social_worker(child) }.sort
+  end
+
+  def unrecorded_arrivals
+    @tasks = Child.unrecorded_arrivals.map { |child| Task.record_arrival(child) }.sort
   end
 
   def upcoming_home_visits
