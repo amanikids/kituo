bd6022f08b6e3f5aeb758a077edf3dfff1decc88
Matthew Todd <matthew.todd@gmail.com>
2009-05-25 11:03:06 +0300

    Do away with calculated instance variables.

--- app/presenters/bar_graph.rb
+++ app/presenters/bar_graph.rb
@@ -1,17 +1,12 @@
 class BarGraph
-  attr_reader :partitioned_data
-
   def initialize(statistic, number_of_bars)
-    @width = 400
-    @height = 200
-    @label_height = 16
-    @height_for_bars = @height - @label_height
-    @number_of_bars  = number_of_bars
-    @label_interval  = 2
+    @statistic      = statistic
+    @number_of_bars = number_of_bars
 
-    @partitioned_data = statistic.partitioned(@number_of_bars)
-    @width_of_one_bar = @width / @number_of_bars
-    @height_of_one_unit = @height_for_bars / @partitioned_data.data.values.max
+    @width          = 400
+    @height         = 200
+    @label_height   = 16
+    @label_interval = 2
   end
 
   class Bar < Struct.new(:x, :y, :width, :height, :value, :label, :label_height, :show_label)
@@ -21,15 +16,33 @@ class BarGraph
   end
 
   def bars
-    @partitioned_data.data.map do |key, value|
-      bar_number = key / @partitioned_data.partition_size
+    partitioned_data.data.map do |key, value|
+      bar_number = key / partitioned_data.partition_size
 
-      x = (bar_number - 1) * @width_of_one_bar
-      height = value * @height_of_one_unit
-      y = @height_for_bars - height
+      x = (bar_number - 1) * width_of_one_bar
+      height = value * height_of_one_unit
+      y = height_for_bars - height
       show_label = bar_number % @label_interval == 0
 
-      Bar.new(x, y, @width_of_one_bar, height, value, key, @label_height, show_label)
+      Bar.new(x, y, width_of_one_bar, height, value, key, @label_height, show_label)
     end
+  end
+
+  private
+
+  def height_for_bars
+    @height - @label_height
+  end
+
+  def height_of_one_unit
+    height_for_bars / partitioned_data.data.values.max
+  end
+
+  def partitioned_data
+    @statistic.partitioned(@number_of_bars)
+  end
+
+  def width_of_one_bar
+    @width / @number_of_bars
   end
 end
