28d3b466914752e5a163f0299fc723d5b223e571
Matthew Todd <matthew.todd@gmail.com>
2009-05-26 10:02:57 +0300

    This makes it a little easier to play around with the graph.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,11 +1,11 @@
 class StatisticsController < ApplicationController
   def length_of_stay
-    @bar_graph = BarGraph.new(Child.all.map(&:length_of_stay).compact.map(&days_to_months))
+    @bar_graph = BarGraph.new(Child.all.map(&:length_of_stay).compact.map(&divide_by(30)))
   end
 
   private
 
-  def days_to_months
-    lambda { |days| (days / 30).floor }
+  def divide_by(denominator)
+    lambda { |numerator| (numerator / denominator).floor }
   end
 end
