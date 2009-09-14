589f91f88824dad880b6a7520d9d4558804a4b33
Xavier Shay <xavier@rhnh.net>
2009-09-14 11:36:48 +0300

    Inline change schedule visit for child

--- public/javascripts/child-edit.js
+++ public/javascripts/child-edit.js
@@ -1,16 +1,41 @@
 $(function() {
-  var form_elements = 'label, input, select, .buttons'
+  var form_elements = $('#child-form label, #child-form input, #child-form select, .buttons');
   $(form_elements).hide();
 
   $('.edit').click(function() {
     $(form_elements).show();
-    $('.editable').hide();
+    $('#child-form .editable, .edit .editable').hide();
     return false;
   });
 
   $('.buttons .cancel').click(function() {
     $(form_elements).hide();
-    $('.editable').show();
+    $('#child-form .editable, .edit .editable').show();
     return false;
+  });
+
+  $('.buttons .save').click(function() {
+    $('#child-form').submit();
+    return false;
+  });
+
+
+
+
+  $('.change').click(function() {
+    $('.child-visits .editable').hide();
+    $('.date-field').show();
+    $('.date-field').datepicker('show');
+    return false;
+  });
+
+  picker = $('.date-field').datepicker({
+    dateFormat: 'd M yy',
+    firstDay: 1,
+    onClose: function() {
+      $('.child-visits .editable').text($('.date-field').attr('value'));
+      $('.child-visits .editable').show();
+      $('.date-field').hide();
+    }
   });
 });
