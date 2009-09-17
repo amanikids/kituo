4dce127fc3308d1182006cf8fc532d174e6f9b2b
Xavier Shay <xavier@rhnh.net>
2009-09-17 10:15:38 +0300

    Clean up sign out, still needs a bit of styling

--- features/acceptance/step_definitions/user_steps.rb
+++ features/acceptance/step_definitions/user_steps.rb
@@ -9,5 +9,7 @@ end
 
 Given /^I am not signed in$/ do
   visit path_to("the english dashboard")
-  click_button("sign out")
+  unless current_url.ends_with?("/session/new")
+    click_link("not you?")
+  end
 end
