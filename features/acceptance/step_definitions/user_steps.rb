27484d84da3b78b2f8022b7a16a46b56c137e5d7
Matthew Todd <matthew.todd@gmail.com>
2009-09-17 11:34:41 +0300

    Adjust the layout for the "not you?" link.

--- features/acceptance/step_definitions/user_steps.rb
+++ features/acceptance/step_definitions/user_steps.rb
@@ -10,6 +10,6 @@ end
 Given /^I am not signed in$/ do
   visit path_to("the english dashboard")
   unless current_url.ends_with?("/session/new")
-    click_link("not you?")
+    click_link("Not you?")
   end
 end
