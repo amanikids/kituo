0b1efac8bbef395ce6b442a9dc9c3214314df5ac
Matthew Todd <matthew.todd@gmail.com>
2009-09-17 09:26:49 +0300

    Preserve pristine Capfile -- put all our stuff in deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,10 @@
+require 'vendor/plugins/action_mailer_optional_tls/lib/smtp_tls'
+require 'vendor/plugins/action_mailer_optional_tls/lib/action_mailer_tls'
+require 'vendor/plugins/cap_gun/lib/cap_gun'
+
+# =============================================================================
+# = Settings                                                                  =
+# =============================================================================
 set :application, 'kituo'
 set :repository,  '/var/www/apps/git/kituo.git'
 set :local_repository,  'deploy@mchungaji.amani:git/kituo.git'
@@ -37,3 +44,64 @@ set :cap_gun_email_envelope, {
   :recipients => %w[joe@amanikids.org japhary@amanikids.org matthew.todd@gmail.com],
   :from       => 'Deployment Notifier <deploy@amanikids.org>'
 }
+
+# =============================================================================
+# = Tasks                                                                     =
+# =============================================================================
+task :production do
+  set :rails_env, 'production'
+  set :domain, 'http://kituo.amani'
+end
+
+namespace :deploy do
+  desc 'Deploy the app, running migrations.'
+  task :default do
+    migrations
+  end
+
+  desc 'Start the Application'
+  task :start do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task(:stop) { }
+
+  desc 'Restart the Application'
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+# =============================================================================
+# = Callbacks                                                                 =
+# =============================================================================
+after 'deploy:setup' do
+  database_config = {
+    rails_env => {
+      'adapter' => 'mysql',
+      'encoding' => 'utf8',
+      'database' => "kituo_#{rails_env}",
+      'username' => 'root',
+      'password' => '',
+      'socket' => '/var/run/mysqld/mysqld.sock'
+    }
+  }
+  put database_config.to_yaml, "#{shared_path}/database.yml"
+end
+
+after 'deploy:update_code' do
+  run "ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml"
+
+  if rails_env == 'staging'
+    # Replace our data with production data.
+    run "mysqladmin --user root drop --force kituo_#{rails_env}"
+    run "mysqladmin --user root create kituo_#{rails_env}"
+    run "mysqldump  --user root --opt kituo_production | mysql --user root kituo_#{rails_env}"
+
+    # Replace our system files (think Paperclip) with production system files.
+    run "rm -rf #{shared_path}/system"
+    run "cp -r  #{shared_path.gsub('staging', 'production')}/system #{shared_path}"
+  end
+end
+
+after 'deploy:restart', 'cap_gun:email'
