fac7df166cbec1c26fda0cc20b5a262ea9c721a3
Xavier Shay <xavier@rhnh.net>
2009-09-17 11:22:55 +0300

    Show a global ajax working indicator

--- public/javascripts/ajax_request_count.js
+++ public/javascripts/ajax_request_count.js
@@ -1,16 +1,33 @@
 // extend jQuery ajax with the capability of remembering the count of active ajax requests
 $.activeAjaxRequestCount = 0;
 
+var timeout = null;
+function toggleLoadIndicator() {
+  $('#ajax-loading').stop();
+  if ($.activeAjaxRequestCount > 0) {
+    timeout = setTimeout(function() {
+      $('#ajax-loading').fadeIn();
+    }, 200);
+  } else {
+    clearTimeout(timeout);
+    $('#ajax-loading').fadeOut();
+  }
+}
+
 $().ajaxSend(function() {
+
   $.activeAjaxRequestCount++;
+  toggleLoadIndicator();
   // register lazily to make sure it's the last one and get executed after all other handlers
   if (!$._ajaxErrorHandlerAdded) {
     $().ajaxError(function() {
       $.activeAjaxRequestCount--;
+      toggleLoadIndicator();
     });
     $._ajaxErrorHandlerAdded = true;
   }
 })
 .ajaxSuccess(function() {
   $.activeAjaxRequestCount--;
+  toggleLoadIndicator();
 });
