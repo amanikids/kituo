162169435a9b0f8631a95746456c3ccbb3a46a6a
Xavier Shay <xavier@rhnh.net>
2009-09-18 15:12:49 +0300

    L shortcut only fires when no metakey is depressed

--- public/javascripts/change_locale.js
+++ public/javascripts/change_locale.js
@@ -1,6 +1,6 @@
 $(function() {
   $("body").keypress(function (e) {
-    if (e.which == 108) {
+    if (e.which == 108 && e.metaKey == false) {
       var current_location = window.location.toString();
       window.location = current_location + '?next_locale'
     }
