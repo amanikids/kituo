4290fcf432cc77d51bc229e012976b21d81fa284
Matthew Todd <matthew.todd@gmail.com>
2009-09-22 16:36:12 +0300

    Random polishing.

--- public/javascripts/inline_edit.js
+++ public/javascripts/inline_edit.js
@@ -1,13 +1,16 @@
 $(function() {
-  var inlineEditLink = function(element, toShow, toHide) {
+  var inlineEditLink = function(element, toShow, toHide, callback) {
     $(element).click(function() {
       var form = $(this).parents('form');
       form.find(toHide).hide();
       form.find(toShow).show();
+      if (callback) {
+        callback(form);
+      }
       return false;
     });
   }
-  inlineEditLink('#cancel-link', '.show', '.edit');
+  inlineEditLink('#cancel-link', '.show', '.edit', function(form) { form.reset(); });
   inlineEditLink('#edit-link',   '.edit', '.show');
 
   // TODO: This is overridden by lib/form_builder.rb, so doesn't work.
