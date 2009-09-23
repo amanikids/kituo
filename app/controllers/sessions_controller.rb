e34f102cde08c7a53d8beff9eacf53e7c39172f1
Xavier Shay <xavier@rhnh.net>
2009-09-23 15:39:16 +0300

    Class level comment for sessions controller about why we don't have passwords

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,6 @@
+# We don't require a password for sign in - since everyone can do anything
+# anyways. It's in your own best interest to sign in because then you see
+# a dashboard that is tailored for you.
 class SessionsController < ApplicationController
   def new
     @caregivers = Caregiver.by_name
