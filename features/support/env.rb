ec5c3dfda0d3c27591bea95aa2f7decf135267aa
Xavier Shay <xavier@rhnh.net>
2009-09-23 12:04:21 +0300

    Remove acceptance/integration split between features

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,5 @@
 ENV['RAILS_ENV'] = 'test'
-require File.join(File.dirname(__FILE__), '..', '..', '..', 'config', 'environment')
+require File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment')
 require Rails.root.join('test', 'blueprints')
 require 'cucumber/rails/world'
 require 'cucumber/formatter/unicode' # Comment out this line if you don't want Cucumber Unicode support
