1c3ab46348a481f2aab60efd2a2d0822cb7d050a
Xavier Shay <xavier@rhnh.net>
2009-09-23 11:49:52 +0300

    Remove old code

--- app/controllers/caregivers/base_controller.rb
+++
@@ -1,9 +0,0 @@
-class Caregivers::BaseController < ApplicationController
-  before_filter :load_caregiver
-
-  private
-
-  def load_caregiver
-    @caregiver = Caregiver.find(params[:caregiver_id])
-  end
-end
--- app/models/case_assignment.rb
+++
@@ -1,5 +0,0 @@
-class CaseAssignment < ActiveRecord::Base
-  belongs_to :child
-  belongs_to :social_worker, :class_name => 'Caregiver'
-  attr_accessible :child_id, :social_worker_id
-end
