a38dfc51dbd64e7657a5d7d150eca835b712acbc
Matthew Todd <matthew.todd@gmail.com>
2009-09-24 11:38:18 +0300

    Use command-\ to change locale, not l.

--- public/javascripts/change_locale.js
+++ public/javascripts/change_locale.js
@@ -1,6 +1,6 @@
 $(function() {
   $("body").keypress(function (e) {
-    if (e.which == 108 && e.metaKey == false) {
+    if (e.which == 92 && e.metaKey == true) {
       window.location = $("link.locale").attr("href");
     }
   })
