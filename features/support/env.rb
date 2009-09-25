96925e7b00b8a0ab9f61a2dae6c201f17bac72c4
Matthew Todd <matthew.todd@gmail.com>
2009-09-25 09:02:26 +0300

    Update env.rb to work with latest cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,9 +3,9 @@ require File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment')
 require Rails.root.join('test', 'blueprints')
 require 'cucumber/rails/world'
 require 'cucumber/formatter/unicode' # Comment out this line if you don't want Cucumber Unicode support
-Cucumber::Rails.bypass_rescue # Comment out this line if you want Rails own error handling
 
-require 'webrat'
+ActionController::Base.allow_rescue = false
+
 require 'webrat/integrations/selenium'
 
 Webrat.configure do |config|
