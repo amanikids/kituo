d2bc4a21d48d896e0186d4bb12639eba3db2af0f
Matthew Todd <matthew.todd@gmail.com>
2009-10-05 12:08:55 +0300

    Caregiver is the only model name we care to translate. And it's in a different noun class than the default.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,6 +39,16 @@ module ApplicationHelper
     link_to_with_current name, request.path.include?("/#{segment}"), options, html_options
   end
 
+  def scope_for_noun(model, parent_scope)
+    return parent_scope unless I18n.locale.to_sym == :sw
+
+    noun_scope = {
+      Caregiver => :mwa
+    }.fetch(model.class)
+
+    parent_scope + [noun_scope]
+  end
+
   def other_locales
     other_locales = {}.with_indifferent_access
     I18n.available_locales.each { |locale| other_locales[locale] = t(:locale_name, :locale => locale) }
