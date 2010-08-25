7b8ed6e25708710f774ccca228bebaabc10189eb
Matthew Todd <matthew.todd@gmail.com>
2010-08-25 15:22:41 +0300

    Update prawn.

--- test/functional/children_controller_test.rb
+++ test/functional/children_controller_test.rb
@@ -5,6 +5,9 @@ class ChildrenControllerTest < ActionController::TestCase
     setup do
       @expected_children = (1..2).map { Child.make(:state => 'on_site') }
       chaff = Child.make(:state => 'reunified')
+      # prawnto doesn't take into account that this setting might be nil, so we
+      # have to set it in the test.
+      @request.env['SERVER_PROTOCOL'] = 'http'
       get :on_site, :format => :pdf
     end
 
