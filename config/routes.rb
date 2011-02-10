885e02bab5396e121d253adf6592d7a80df9eec6
Matthew Todd <matthew@matthewtodd.org>
2011-02-10 19:13:48 +0300

    Straighten up the routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,27 @@
 ActionController::Routing::Routes.draw do |map|
   map.filter :locale
 
-  map.length_of_stay_statistics '/statistics/length_of_stay.svg', :controller => 'statistics', :action => 'length_of_stay', :format => :svg, :conditions => { :method => :get }
+  map.length_of_stay_statistics '/statistics/length_of_stay.svg',
+    :controller => 'statistics',
+    :action     => 'length_of_stay',
+    :format     => :svg,
+    :conditions => {
+      :method => :get }
 
   map.resource :session
+
   map.resources :children,
-    :collection => {:on_site => :get},
-    :member     => {:resolve_duplicate => :put}
+    :collection => {
+      :on_site => :get },
+    :member => {
+      :resolve_duplicate => :put }
+
   map.resources :caregivers
   map.resources :events
-  map.resources :scheduled_visits, :member => {:complete => :put}
-  map.root :controller => 'dashboard', :action => 'show'
+  map.resources :scheduled_visits,
+    :member => {
+      :complete => :put }
+
+  map.root :controller => 'dashboard',
+    :action => 'show'
 end
