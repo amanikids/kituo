2b204f8d92535abf0d9176ec010f497c20417c3f
Matthew Todd <matthew@matthewtodd.org>
2011-02-10 18:21:02 +0300

    Whack old performance test.

--- test/performance/statistics_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-require 'performance_test_help'
-
-# Profiling results for each test method are written to tmp/performance.
-class StatisticsTest < ActionController::PerformanceTest
-  def test_length_of_stay
-    get length_of_stay_statistics_path(:format => :svg)
-  end
-end
