1de6d48c55dfe56d849d55b442e047b73a4e69ee
Matthew Todd <matthew@matthewtodd.org>
2011-02-10 19:21:31 +0300

    Consolidate steps that perform actions.

--- features/step_definitions/action_steps.rb
+++ features/step_definitions/action_steps.rb
@@ -1,3 +1,8 @@
+When /^I follow "([^\"]*)" for the event that happened on "([^\"]*)"$/ do |link, day|
+  selector = "tr[data-happened_on='#{parse_date(day).to_s(:db)}']"
+  When %{I follow "#{link}" within "#{selector}"}
+end
+
 When /^I drag "([^\"]*)" to "([^\"]*)"$/ do |child_name, date|
   date = parse_date(date).to_date.to_s(:db)
   source = page.find(:xpath, "//a[contains(text(),'#{child_name}')]/ancestor::li")
--- features/step_definitions/event_steps.rb
+++
@@ -1,5 +0,0 @@
-When /^I follow "([^\"]*)" for the event that happened on "([^\"]*)"$/ do |link, day|
-  selector = "tr[data-happened_on='#{parse_date(day).to_s(:db)}']"
-  When %{I follow "#{link}" within "#{selector}"}
-end
-
