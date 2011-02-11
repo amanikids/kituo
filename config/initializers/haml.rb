56f5b5e9ff215f20b31a5ffc8cad75b585b058e9
Matthew Todd <matthew@matthewtodd.org>
2011-02-11 14:37:29 +0300

    That wasn't true.

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,7 +1,3 @@
-# Apparently you have to say this when Haml's a vendored gem but not installed
-# outside this project?
-Haml.init_rails(binding)
-
 # Tell Haml to wrap html attributes with double instead of single quotes.
 Haml::Template.options[:attr_wrapper] = '"'
 
